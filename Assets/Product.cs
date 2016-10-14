8125869b00e2f390cb40c5d86eba4e6fb96e1147
wsbobsw <wsbobsw@163.com>
2016-10-15 11:04:11 +1300

    xzha439

--- Assets/Product.cs
+++ Assets/Product.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Text;
 
 //Make sure you get rid of the namespace that is automatically generated here.
-class employee
+class Product
 {
     //List all the columns of your table here in the same format as below.
-    public string employeeID { get; set; }
-    public string name { get; set; }
-
+    public string ProductID { get; set; }
+    public string Manufacturer { get; set; }
+    public string ProductName { get; set; }
+    public string Segment { get; set; }
 }
